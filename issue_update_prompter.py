c547ccd6fe9dc6718d88b53acfa37269e7d70311
Brian Coulter <bcoulter@rentjungle.com>
2017-12-20 10:48:03 -0500

    dynamic config loc and bunches
    I added a 4th optional arguement to be able to specify the config.yaml
    fullpath at run time. This was pretty simple. At some point, we
    should add schema validation of the yaml file though, so the
    code does not at erratically in response to mis-spellings or
    formatting in that file.
    
    I also change the timeframe structure into a bunch to slightly simplify the
    syntax at the lower levels.

--- issue_update_prompter.py
+++ issue_update_prompter.py
@@ -1,6 +1,7 @@
 import dominate
 import itertools
 import copy
+import bunch
 import yaml
 from datetime import datetime
 from github import Github
@@ -37,10 +38,27 @@ def time_to_notify(timeframe, last_updated):
     target_days (int or float): number of days at which we want to notify
     last_updated (datetime): datetime object from github of last time issues was updated """
     day_delta = (datetime.now() - last_updated).days
-    if 'catch-up' in timeframe and timeframe['catch-up']:
-        return day_delta >= timeframe['days']
+    if 'catch-up' in timeframe and timeframe.catch-up:
+        return day_delta >= timeframe.days
     else:
-        return day_delta == timeframe['days']
+        return day_delta == timeframe.days
+
+
+def generate_recipients(timeframe, issue):
+    recipients = copy.deepcopy(timeframe.recipients)
+    if 'AUTHORS' in recipients:
+        def debug_assignees(assignee):
+            if assignee.email is None:
+                print('''Github user {} can't be contacted as they don't 
+                      have a public email set. Found in issue {}.'''.format(assignee, issue))
+
+        list(map(debug_assignees, issue.assignees))
+        assignee_emails = [assignee.email for assignee in issue.assignees if assignee.email]
+        if not assignee_emails:
+            pass
+            # todo: what do we want to do with issues that are not assigned? PM or nothing?
+        recipients.remove('AUTHORS')
+        recipients += assignee_emails
 
 
 def get_notification_from_issues(criteria, issues):
@@ -51,21 +69,9 @@ def get_notification_from_issues(criteria, issues):
         target_labels = issue_labels & notification_labels
         for target_label in target_labels:
             # reversed sorted order so if there is a different notification level at 60 and one at 30, only the 60 is sent
-            for timeframe in sorted(criteria[target_label], key=lambda x: x['days'], reverse=True):
+            for timeframe in sorted(criteria[target_label], key=lambda x: x.days, reverse=True):
                 if time_to_notify(timeframe, issue.updated_at):
-                    recipients = copy.deepcopy(timeframe['recipients'])
-                    if 'AUTHORS' in recipients:
-                        def debug_assignees(assignee):
-                            if assignee.email is None:
-                                print('''Github user {} can't be contacted as they don't 
-                                      have a public email set. Found in issue {}.'''.format(assignee, issue))
-                        list(map(debug_assignees, issue.assignees))
-                        assignee_emails = [assignee.email for assignee in issue.assignees if assignee.email]
-                        if not assignee_emails:
-                            pass
-                            # todo: what do we want to do with issues that are not assigned? PM or nothing?
-                        recipients.remove('AUTHORS')
-                        recipients += assignee_emails
+                    recipients = generate_recipients(timeframe, issue)
                     if recipients:
                         notifications.append({'issue': issue, 'recipients': recipients})
                     break
@@ -89,14 +95,17 @@ def process_notification_data(notification_data):
                 timeframe['recipients'] = notification_data['severity'][severity]
                 if 'catch-up' in notification_data and notification_data['catch-up']:
                     timeframe['catch-up'] = True
+            notification_data['criteria'][label] = list(map(bunch.Bunch, timeframes))
     else:
         raise(Exception('This system cannot function without notification "criteria" being specified'))
     return notification_data['criteria']
 
 
-def get_notification_criteria():
-    # todo: make config file dynamic
-    with open('config.yaml', 'r') as yaml_file:
+def get_notification_criteria(config_file_path=None):
+    # todo: should use a schema file to make sure format is correct
+    if config_file_path is None:
+        config_file_path = 'config.yaml'
+    with open(config_file_path, 'r') as yaml_file:
         notification_data = yaml.load(yaml_file)
     criteria = process_notification_data(notification_data)
     return criteria
@@ -139,15 +148,19 @@ def main(args=None):
         args = arguments.Args()
         if len(args) < 3:
             print('Sends email reminders for Github Issues that need updates.')
-            print('Usage: python report.py <GITHUB_TOKEN> <GITHUB_REPO> <GITHUB_ORG>')
+            print('Usage: python report.py <GITHUB_TOKEN> <GITHUB_REPO> <GITHUB_ORG> [<CONFIG_FILE_PATH>]')
             exit(1)
     token = args.get(0)
     repository = args.get(1)
     organization = args.get(2)
+    if len(args) > 3:
+        config_file_path = args.get(3)
+    else:
+        config_file_path = None
     if token is not None and repository is not None and organization is not None:
         g = Github(token)
         repos = g.get_organization(organization).get_repos(repository)
-    criteria = get_notification_criteria()
+    criteria = get_notification_criteria(config_file_path)
     issue_notifications = get_notification_from_repos(criteria, repos)
     emails = sort_issue_notifications_into_emails(issue_notifications)
     for email in emails:
