6b622d7e07739399853c92426727c1535066f71e
Brian Coulter <bcoulter@rentjungle.com>
2018-03-19 16:23:30 -0400

    updating reminder name change in run scripts

--- utils/send_test_emails.bash
+++ utils/send_test_emails.bash
@@ -1,2 +1,2 @@
 source utils/env.sh
-~/.virtualenvs/github_issues_report/bin/python issue_update_prompter.py $GITHUB_TOKEN $GITHUB_REPO $GITHUB_ORG --send_to_test_target $TEST_EMAIL_TARGET
+~/.virtualenvs/github_issues_report/bin/python issue_reminders.py $GITHUB_TOKEN $GITHUB_REPO $GITHUB_ORG --send_to_test_target $TEST_EMAIL_TARGET
